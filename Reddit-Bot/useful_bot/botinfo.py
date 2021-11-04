612d6476e7f75ff582010d6d5e9e5f6464f83e1d
Nguyễn Thanh Châu <69909023+thanhchauns2@users.noreply.github.com>
2021-11-04 20:02:29 +0700

    Update botinfo.py

--- Reddit-Bot/useful_bot/botinfo.py
+++ Reddit-Bot/useful_bot/botinfo.py
@@ -1,11 +1,11 @@
 # External
 import logging
 
-client_id = "sTtNLk_LuariTIscYq7ATQ"
-client_secret = "FpcfQMeQ661RMPwgn9PAYSYPgF4dVA"
-password = "thanhchauns2"
-username = "Fantastic_Editor_702"
-author = "Fantastic_Editor_702"
+client_id = ""
+client_secret = ""
+password = ""
+username = ""
+author = ""
 user_agent = "/u/" + username + " by /u/" + author
 subreddit = "nosleep"
 logging_level = logging.DEBUG  # Levels are DEBUG, INFO, WARNING, ERROR, CRITICAL. Recommended level is INFO
